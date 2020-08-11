380f893e6cfe30715c7b22154b041fa5e61ab2eb
Daniel Wills <daniel.wills83@gmail.com>
2020-08-11 00:09:03 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -25,7 +25,7 @@ function destructivelyAddElementToEndOfArray(myArray) {
   return myArray
 }
 
-function accessElementInArray() {
+function accessElementInArray(someArray, 3) {
   var someArray = ['black', 'blue', 'red', 'green']
   var someColor = someArray[3]
   return someColor
